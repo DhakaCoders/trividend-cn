f369cdfeea5e8c3bfc8bb722980b357495837a5a
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2021-02-08 19:03:45 +0600

    ,

--- themes/trividend/assets/js/main.js
+++ themes/trividend/assets/js/main.js
@@ -349,6 +349,18 @@ google.maps.event.addDomListener(window, 'load', initialize);
         speed: 300,
         slidesToShow: 5,
         slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       });
     }
 
@@ -361,6 +373,21 @@ google.maps.event.addDomListener(window, 'load', initialize);
         $(this).parent().siblings().find('.ftr-menu-des').slideUp(300);
       });
 
+    }
+
+    if(windowWidth <= 767){
+      if($('.bnwsSlider').length ){
+        $('.bnwsSlider').slick({
+          dots: true,
+          infinite: false,
+          autoplay: false,
+          arrows: false,
+          autoplaySpeed: 2000,
+          speed: 300,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        });
+      }
     }
 
 
