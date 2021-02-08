bf9324a436f37d36654080c072d04a7497b3fbbf
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2021-02-08 16:36:18 +0600

    ,

--- themes/trividend/assets/js/main.js
+++ themes/trividend/assets/js/main.js
@@ -309,6 +309,25 @@ google.maps.event.addDomListener(window, 'load', initialize);
           slidesToScroll: 1,
           prevArrow: $('.tvd-nieuws-slider-ctlr .fl-nxt-prev .fl-prev'),
           nextArrow: $('.tvd-nieuws-slider-ctlr .fl-nxt-prev .fl-next'),
+          responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
         });
     }
 
