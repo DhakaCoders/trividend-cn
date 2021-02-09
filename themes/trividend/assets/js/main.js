e4a86fbc69312e02c3115184e3d217e6e0f76d6d
monirulislamweb <65311288+monirulislamweb@users.noreply.github.com>
2021-02-09 12:44:53 +0600

    ...

--- themes/trividend/assets/js/main.js
+++ themes/trividend/assets/js/main.js
@@ -204,6 +204,12 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
   /* Start of Noyon */
 
+  if( $('.humberger-icon').length ){
+    $('.humberger-icon').click(function(){
+      $(this).parent().toggleClass('close-icon');
+    });
+  }
+
   if( $('.hdr-search').length ){
     $('.hdr-search button').click(function(e){
       event.preventDefault();
@@ -231,34 +237,6 @@ google.maps.event.addDomListener(window, 'load', initialize);
       slidesToScroll: 1,
       prevArrow: $('.hm-banner-cntlr .fl-nxt-prev .fl-prev'),
       nextArrow: $('.hm-banner-cntlr .fl-nxt-prev .fl-next'),
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
     });
 }
 
