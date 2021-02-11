9b87d4aefbed7ffcf7b48a02de953b5551f0e2b2
Foridul Islam <foridulsumon@gmail.com>
2021-02-11 21:12:01 +0600

    gu

--- themes/trividend/404.php
+++ themes/trividend/404.php
@@ -6,8 +6,14 @@ get_header();
         <div class="row">
             <div class="col-sm-12">
                 <div class="page-404 text-center">
+                    <span style="transform: rotate(180deg); display: inline-block;">
+                      <i><img src="<?php echo THEME_URI; ?>/assets/images/team-border-btm.svg"></i>
+                    </span>
                     <h1>404</h1>
-                    <h3><?php _e( 'It looks like nothing was found at this location.', THEME_NAME ); ?></h3>
+                    <p><?php _e( 'It looks like nothing was found at this location.', THEME_NAME ); ?></p>
+                    <span>
+                      <i><img src="<?php echo THEME_URI; ?>/assets/images/team-border-btm.svg"></i>
+                    </span>
                 </div>
             </div>
         </div>
