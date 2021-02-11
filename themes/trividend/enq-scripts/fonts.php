f43d89cd7cf8cea7b89149c78e94cd62443b987a
Rannojit <cserannojitsarkar22@gmail.com>
2021-02-11 14:53:13 +0600

    .

--- themes/trividend/enq-scripts/fonts.php
+++ themes/trividend/enq-scripts/fonts.php
@@ -1,6 +1,5 @@
 <?php 
 wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/fonts/font-awesome/font-awesome.css', array(), '5.5.0');
 wp_enqueue_style('cbv-custom.fonts', get_template_directory_uri() . '/assets/fonts/custom-fonts.css', array(), '1.0.0');
-wp_enqueue_style('cbv-fonts-montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', array(), '1.0.0');
-wp_enqueue_style('cbv-fonts-titillium', 'https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap', array(), '1.0.0');
+wp_enqueue_style('cbv-fonts-FiraSans', 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', array(), '1.0.0');
 ?>
